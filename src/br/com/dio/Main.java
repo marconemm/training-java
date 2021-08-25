210311f7d3b36a948dc05bb6408eb621ce6c592c
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-08-25 19:47:50 -0300

    Initial commit

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.models.Dog;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		
+		System.out.println(dog);
+
+	}
+
+}
